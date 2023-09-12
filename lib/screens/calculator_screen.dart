f844642194d6d87dd024c902e98bcc7fa4df2855
egebakisoglu <egebakisoglu@gmail.com>
2023-09-12 20:09:58 +0300

    added buttons to ui

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:calculator/models/calculator_model.dart';
@@ -9,23 +10,25 @@ class CalculatorScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     CalculatorModel calculatorModel = ref.watch(calculatorProvider);
 
+    final List<String> buttons = [
+      "CE", "C", "%", "/",
+      "7", "8", "9", "x",
+      "4", "5", "6", "-",
+      "1", "2", "3", "+",
+      "0", "+/-", ",", "="
+    ];
+
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.all(15),
-          reverse: true,
-          physics: const NeverScrollableScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Container(
               alignment: Alignment.bottomRight,
-              height: 85,
+              height: 120,
               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              margin: EdgeInsets.symmetric(horizontal: 15),
-              decoration: BoxDecoration(
-                color: Colors.grey[800],
-                borderRadius: BorderRadius.circular(12),
-              ),
+              margin: EdgeInsets.symmetric(horizontal: 30),
               child: Expanded(
                 child: SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
@@ -34,12 +37,43 @@ class CalculatorScreen extends ConsumerWidget {
                     calculatorModel.input,
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 60,
+                      fontSize: 90,
                     ),
                   ),
                 ),
               ),
             ),
+            Container(
+              margin: EdgeInsets.only(bottom: 30, right: 10, left: 10),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4
+                ),
+                shrinkWrap: true,
+                itemCount: buttons.length,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0 || index == 1 || index == 2) {
+                    return MyButton(
+                      color: Colors.grey[400],
+                      textColor: Colors.black,
+                      text: buttons[index],
+                    );
+                  }
+                  else if (index == 3 || index == 7 || index == 11 || index == 15 || index == 19){
+                    return MyButton(
+                      color: Colors.orange,
+                      textColor: Colors.white,
+                      text: buttons[index],
+                    );
+                  }
+                  return MyButton(
+                    color: Colors.grey[900],
+                    textColor: Colors.white,
+                    text: buttons[index],
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final color;
+  final textColor;
+  final String text;
+
+  const MyButton({
+    super.key,
+    required this.color,
+    required this.textColor,
+    required this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:,
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
