d91393dd21105d737c5279e8fd7d996ec0b920fb
egebakisoglu <egebakisoglu@gmail.com>
2023-09-12 22:36:37 +0300

    added onTap parameter

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -4,29 +4,34 @@ class MyButton extends StatelessWidget {
   final color;
   final textColor;
   final String text;
+  final Function()? onTap;
 
   const MyButton({
     super.key,
     required this.color,
     required this.textColor,
     required this.text,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(15), bottom: Radius.circular(8)),
-        color: color,
-      ),
-      child: Center(
-        child: Text(
-          text,
-          style: TextStyle(
-            color: textColor,
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(15), bottom: Radius.circular(8)),
+          color: color,
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
